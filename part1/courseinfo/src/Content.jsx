53be485bd8977c2995b3297af6b3c27e5ffd04e6
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-07-10 13:43:37 +0300

    1.2: course information, step2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,3 +1,5 @@
+import { Part } from "./Part";
+
 const Content = ({
   part1, exercises1,
   part2, exercises2,
@@ -5,15 +7,12 @@ const Content = ({
 }) => {
 
   return <>
-    <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1}
+        exercise={exercises1} />
+      <Part part={part2}
+        exercise={exercises2} />
+      <Part part={part3}
+        exercise={exercises3} />
   </>
 }
 
