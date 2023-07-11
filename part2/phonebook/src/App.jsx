2f1d9ea38e98d3cf4fce75d10db41340a5085902
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-07-11 16:35:59 +0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,25 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '1234-123' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
 
+    if(persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -32,13 +42,17 @@ const App = () => {
             onChange={handleNewNameChange} />
         </div>
         <div>
+          number: <input value={newNumber}
+            onChange={handleNewNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(note => 
-          <li key={note.id}>{note.name}</li>
+          <li key={note.id}>{note.name} {note.number}</li>
         )}
       </ul>
     </div>
