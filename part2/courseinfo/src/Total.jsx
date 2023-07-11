cf531980cf1ef3c87f6f81818bdd4425cac6017b
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-07-11 15:50:11 +0300

    use only reduce

--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -1,5 +1,6 @@
 const Total = ({ parts }) => {
-  const total = parts.map((part) => part.exercises).reduce((sum, exercise) => sum + exercise, 0)
+  const total = parts
+    .reduce((sum, part) => sum + part.exercises, 0)
 
   return <b>total of exercises {total}</b>
 }
