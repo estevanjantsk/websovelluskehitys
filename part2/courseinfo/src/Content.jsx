36c6fda495fc24b1b05f4caee88263ab251c01ac
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-07-11 15:45:10 +0300

    part2 - 2.1: Course information step6

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -1,14 +1,16 @@
-import { Part } from "./Part";
+import { Part } from './Part'
 
 const Content = ({
   parts
 }) => {
 
-  return <>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
-  </>
+  return (
+    <div>
+      {parts.map((part) => <Part key={part.id}
+        part={part} />)
+      }
+    </div>
+  )
 }
 
 export { Content }
